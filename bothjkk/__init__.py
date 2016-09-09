1ab6861a51dbf1a4536faa9e7fe98b18a2010534
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-09 14:40:43 +0300

    qwe

--- bothjkk/__init__.py
+++ bothjkk/__init__.py
@@ -1,3 +1,21 @@
-__author__ = 'student'
-import numpy as np
-y=np.log(1-x**2[])
+import math
+import pylab
+from matplotlib import mlab
+
+xmin = -20.0
+xmax = 20.0
+
+dx = 0.01
+xlist = mlab.frange (xmin, xmax, dx)
+
+pylab.ion()
+
+for n in range (50):
+    ylist = [math.sin (x + n / 2.0) for x in xlist]
+    pylab.clf()
+    pylab.plot (xlist, ylist)
+    pylab.draw()
+    pylab.pause(0.3)
+
+
+pylab.close()
